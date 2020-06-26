98bfb9d5a657d0c182bd775f731d78971f444d08
Rafael Elias Ribeiro Barbosa <rafaeleliasrb@gmail.com>
2020-06-26 13:49:29 -0300

    Update

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import pysftp
+import glob
+import os
+import pandas as pd
+
+local_path = 'local-backup'
+if not os.path.exists(local_path):
+    os.mkdir(local_path)
 
 hostname = "172.20.0.4"
 username = "foo"
@@ -7,14 +14,10 @@ port = 22
 
 cn_opts = pysftp.CnOpts()
 cn_opts.hostkeys = None
-
 with pysftp.Connection(host=hostname, username=username, password=password, port=port, cnopts=cn_opts) as sftp:
-    print ("Connection succesfully stablished ... ")
+    sftp.get_d('upload', local_path, preserve_mtime=True)
 
-    # Switch to a remote directory
-    #sftp.cwd('/var/www/vhosts/')
+all_files = glob.glob(os.path.join(local_path, "*.csv"))
+df_concat_report = pd.concat((pd.read_csv(f, delimiter = ';') for f in all_files))
 
-    print(sftp.listdir())
-    
-    with sftp.cd('upload'):             # temporarily chdir to public
-        print(sftp.listdir())
+df_concat_report.to_csv(os.path.join(local_path, 'cancel_report.csv'), index = False, header=True, sep=";")
