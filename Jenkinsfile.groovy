dd6414a0f67cac68544267071afd60af65cab463
Rafael Elias Ribeiro Barbosa <rafaeleliasrb@gmail.com>
2020-06-28 01:02:17 -0300

    update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,8 +11,14 @@ pipeline {
             }
             
             steps {
-                withEnv(["HOME=${env.WORKSPACE}", "SFTP_USERNAME=${env.SFTP_USERNAME}", "SFTP_PASSWORD=${env.SFTP_PASSWORD}", "SFTP_HOSTNAME=${env.SFTP_HOSTNAME}", 
-                         "SFTP_PORT=${env.SFTP_PORT}" ]) {
+                environment {
+                    HOME = ${env.WORKSPACE}
+                    SFTP_USERNAME = ${env.SFTP_USERNAME}
+                    SFTP_PASSWORD = ${env.SFTP_PASSWORD}
+                    SFTP_HOSTNAME = ${env.SFTP_HOSTNAME}
+                    SFTP_PORT = ${env.SFTP_PORT}
+                }
+                script {
                   sh "pip install -r requirements.txt --user"
                   sh 'python ./test.py'
                 }                
