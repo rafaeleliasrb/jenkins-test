3abc782d8eafc4433645394489acdfbccaf13f74
Rafael Elias Ribeiro Barbosa <rafaeleliasrb@gmail.com>
2020-06-29 10:02:01 -0300

    update

--- test.py
+++ test.py
@@ -2,14 +2,10 @@ import pysftp
 import glob
 import os
 import pandas as pd
-import socket
 
 local_path = 'local-backup'
 if not os.path.exists(local_path):
     os.mkdir(local_path)
-
-ip_address = socket.gethostbyname(os.environ['SFTP_HOSTNAME'])
-print(ip_address)
 
 hostname = os.environ['SFTP_HOSTNAME']
 username = os.environ['SFTP_USERNAME']
@@ -18,7 +14,7 @@ port = os.environ['SFTP_PORT']
 
 cn_opts = pysftp.CnOpts()
 cn_opts.hostkeys = None
-with pysftp.Connection(host=hostname, username=username, password=password, port=port, cnopts=cn_opts) as sftp:
+with pysftp.Connection(host=hostname, username=username, password=password, port=int(port), cnopts=cn_opts) as sftp:
     sftp.get_d('upload', local_path, preserve_mtime=True)
 
 all_files = glob.glob(os.path.join(local_path, "*.csv"))
