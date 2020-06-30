37c418aea95c083db43a79c49a106f59c4e69aa4
Rafael Elias Ribeiro Barbosa <rafaeleliasrb@gmail.com>
2020-06-30 10:57:32 -0300

    Create real pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,8 +2,13 @@ pipeline {
     agent any
 
     stages {
-
-        stage('Tests') {
+        options { timestamps () }
+    environment {
+        FILTERED_FILE="./filtered_data.cvs"
+        FTP_FILE="./ftp_data.cvs"
+    }
+    stages {
+        stage("Download cancelation file") {
             agent {
                 docker {
                     image 'python:3.7'
@@ -17,14 +22,31 @@ pipeline {
                 SFTP_PASSWORD = "pass"
                 SFTP_HOSTNAME = "sftp"
                 SFTP_PORT = "22"
+                DIRECTORY="local-directory"
             }
             
-            steps {
+            steps{
                 script {
-                  sh "pip install -r requirements.txt --user"
-                  sh 'python ./test.py'
-                }                
+                    sendMsgToSlack("Begin the cancellation process...")
+                    sh "pip install -r requirements.txt --user"
+                    sh 'python ./test.py'
+                }
             }
         }
+
+        stage('Upload a CSV') {
+            steps {
+                script {
+                    def inputFile = input message: 'Upload file', parameters: [file(name: "cancel_report.csv", description: 'Upload only CSV file')]
+                    echo "FILE PATH: '${inputFile}'"
+                    def filePath = "${inputFile}"
+                    //saves the file on the workspace
+                    sh "cat ${filePath} > ${FTP_FILE}"
+                    //delete the uploaded file
+                    // inputFile.delete()
+                }
+            }
+        }
+        
     }
 }
